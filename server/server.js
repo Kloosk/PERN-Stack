6fe144d3c203b3d118fcaf233d4a39f086a0f4c2
Lukasz <lukasz.gras.97@gmail.com>
2021-01-26 14:33:12 +0100

    Others routes

--- server/server.js
+++ server/server.js
@@ -18,32 +18,61 @@ app.get("/restaurants", async (req, res) => {
     }
 });
 //get one restaurant
-app.get("/restaurant/:restaurantid",(req,res) =>{
-   //req.params
-   res.status(200).json({
-
-   });
+app.get("/restaurant/:restaurantid", async (req, res) => {
+   try{
+      const {id} = req.params;
+      const {rows} = await db.query(`SELECT * FROM restaurants where id=${id}`);
+      res.status(200).json({
+         data: {
+            restaurants: rows[0]
+         }
+      });
+   }catch (e) {
+      console.log(e);
+   }
 });
 //create restaurant
-app.post("/restaurant",(req,res) =>{
-   //req.body
-   res.status(200).json({
-
-   });
+app.post("/restaurant",async (req,res) =>{
+   const {name,location,price_range} = req.body;
+   try{
+      const {rows} = await db.query(`INSERT INTO restaurants (name,location,price_range) values (${name},${location},${price_range}) returning *`);
+      res.status(200).json({
+         data: {
+            restaurants: rows
+         }
+      });
+   }catch (e) {
+      console.log(e);
+   }
 });
 //update restaurant
-app.put("/restaurant/:restaurantid",(req,res) =>{
-   //req.body
-   res.status(200).json({
-
-   });
+app.put("/restaurant/:restaurantid", async (req, res) => {
+   const {name,location,price_range} = req.body;
+   const {id} = req.params;
+   try{
+      const {rows} = await db.query('UPDATE restaurants SET name=$1, location=$2, price_range=$3 where id=$4 returning *',[name,location,price_range,id]);
+      res.status(200).json({
+         data: {
+            restaurants: rows[0]
+         }
+      });
+   }catch (e) {
+      console.log(e);
+   }
 });
 //delete restaurant
-app.delete("/restaurant/:restaurantid",(req,res) =>{
-   //req.body
-   res.status(200).json({
-
-   });
+app.delete("/restaurant/:restaurantid",async (req, res) => {
+   const {id} = req.params;
+   try{
+      const results = db.query('DELETE FROM restaurants where id=$1',[id]);
+      res.status(204).json({
+         data: {
+            status: "success"
+         }
+      });
+   }catch (e) {
+      console.log(e);
+   }
 });
 
 app.listen(process.env.PORT,() => {
